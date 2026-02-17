95ad204cdf4d2d8ae61015c8df78d29ae7646e09
neri <canisheavenflowao@gmail.com>
2026-02-17 18:20:39 +0900

    サイドメニューデザイン修正

--- themes/bookstyle_v4/header.php
+++ themes/bookstyle_v4/header.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -14,7 +17,8 @@
     <aside class="sidebar">
         <div class="sidebar-logo">
             <a href="<?php echo home_url('/'); ?>" class="logo-box">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="Book Style" class="logo-img">
+                <span>ブックカバーデザイン<br>無料配布サイト</span><br>
+                Book<br>Style
             </a>
         </div>
 
